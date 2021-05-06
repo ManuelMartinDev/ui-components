53a5bb9b60dc8452295e368551c9dc66dc8d40af
ManuelMartinDev <manuelmartindev@outlook.com>
2021-05-06 11:47:25 +0200

    replaced custom native props in input for original htmlInput props

--- src/components/Forms/Inputs/Text/TextInput.tsx
+++ src/components/Forms/Inputs/Text/TextInput.tsx
@@ -2,16 +2,17 @@ import React, { Fragment } from "react";
 import { Input, Label } from "./styles";
 import { theme } from "../../../themes";
 import { shapedComponentsProps } from "../../../commonInterfaces/shapedComponents";
-export interface TextInputProps extends shapedComponentsProps {
+export interface TextInputProps
+  extends shapedComponentsProps,
+    React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   placeholder?: string;
-  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 export const TextInput: React.FC<TextInputProps> = (props) => {
   return (
     <Fragment>
       {props.label && <Label>{props.label}</Label>}
-      <Input name="input" onChange={(e) => alert("changed")} {...props}></Input>
+      <Input name="" onChange={(e) => alert("changed")} {...props}></Input>
     </Fragment>
   );
 };
