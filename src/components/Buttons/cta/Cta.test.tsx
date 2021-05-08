0fc9b6d05528fb925bd65b1eb301e14f95c11b55
ManuelMartinDev <manuelmartindev@outlook.com>
2021-05-08 18:27:20 +0200

    added tests for textinput

--- src/components/Buttons/cta/Cta.test.tsx
+++ src/components/Buttons/cta/Cta.test.tsx
@@ -1,29 +1,13 @@
 import React from "react";
-import { prettyDOM, render, RenderResult } from "@testing-library/react";
+import { render, RenderResult } from "@testing-library/react";
 import { Cta } from "./Cta";
-import userEvent from "@testing-library/user-event";
-import { theme } from "../../themes";
-import { testShapedComponents } from "../../tests/shapedComponents";
 let component: RenderResult;
 beforeEach(() => {
-  component = render(
-    <Cta
-      additionalStyles={{ border: "2px solid red" }}
-      label="Buy now"
-      theme={theme}
-      bgType="primary"
-      hoverBg="red"
-      borderShape="rounded"
-      darkOnUserPreference={true}
-      shadow="big"
-    />
-  );
+  component = render(<Cta label="Buy now" />);
 });
 
 describe("Cta", () => {
   it("should have provided label", () => {
     expect(component.getByText("Buy now")).toBeInTheDocument();
   });
-
-  testShapedComponents(Cta);
 });
---
+++ src/components/Forms/Inputs/Text/TextInput.test.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { prettyDOM, render, RenderResult } from "@testing-library/react";
+import { TextInput } from "./TextInput";
+
+describe("TextInput", () => {
+  it("should have label on top if is given in props", () => {
+    const component = render(
+      <TextInput label="username" labelPosition="top" />
+    );
+    const label = component.container.querySelector("label");
+    expect(label).toHaveStyle("display:block");
+  });
+  it("should have label in a side if is given in props", () => {
+    const component = render(
+      <TextInput label="username" labelPosition="side" />
+    );
+    const label = component.container.querySelector("label");
+    expect(label).toHaveStyle("display:inline-block");
+  });
+  it("has the placeholder given in props", () => {
+    const component = render(
+      <TextInput placeholder="write" label="username" labelPosition="side" />
+    );
+    const input = component.container.querySelector("input");
+    const placeholder = input?.getAttribute("placeholder");
+    expect(placeholder).toBe("write");
+  });
+});
