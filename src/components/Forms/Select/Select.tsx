6a9da162c9a39e3ee027de52537b1b86f12e4d1b
ManuelMartinDev <manuelmartindev@outlook.com>
2021-05-10 16:10:13 +0200

    removed wrong dropdown-items state use case and moved to props

--- src/components/Forms/Select/Select.tsx
+++ src/components/Forms/Select/Select.tsx
@@ -4,16 +4,19 @@ import { shapedComponentsProps } from "../../commonInterfaces/shapedComponents";
 import { uuid } from "uuidv4";
 export interface SelectProps extends shapedComponentsProps {
   dropdownIsOpen?: boolean;
+  itemsList: string[];
 }
 export const SelectDropdown: React.FC<SelectProps> = (props) => {
-  const [itemsList] = useState<string[]>(
-    new Array(10).fill(1).map((e, i) => (e = `Elemento ${i}`))
-  );
   const [dropdownIsOpen, toggleDropdown] = useState<boolean>(false);
+  const { itemsList } = props;
   return (
     <Container>
       <Select {...props} dropdownIsOpen={dropdownIsOpen}>
-        {<Span>{itemsList[0]}</Span>}
+        {React.isValidElement(itemsList[0]) ? (
+          itemsList[0]
+        ) : (
+          <Span>{itemsList[0]}</Span>
+        )}
         <span onClick={() => toggleDropdown(!dropdownIsOpen)}>open me</span>
       </Select>
       {dropdownIsOpen && (
@@ -25,4 +28,8 @@ export const SelectDropdown: React.FC<SelectProps> = (props) => {
       )}
     </Container>
   );
+};
+
+SelectDropdown.defaultProps = {
+  itemsList: new Array(10).fill(1).map((e, i) => (e = `Elemento ${i}`)),
 };
