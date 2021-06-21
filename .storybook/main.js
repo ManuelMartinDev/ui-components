fd49eeb28568426b299399ac816e99640e1c96c1
Manu Web Dev <manuelmartindev@outlook.com>
2021-06-21 12:25:29 +0200

    removed docgen

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,14 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+    "@storybook/preset-create-react-app",
+  ],
+  typescript: {
+    // Disable docgeneration due to TypeScript 2.3.x incompatability.
+    // TODO: Fix this once https://github.com/styleguidist/react-docgen-typescript/issues/356
+    // is addressed
+    reactDocgen: "none",
+  },
+};
